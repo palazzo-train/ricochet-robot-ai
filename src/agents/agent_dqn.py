a20a1ecd08d680e5910fb4f84c48912ef89c0d87
Ray Wong <ray.wkwong@gmail.com>
2018-12-23 23:12:42 +0800

    tuning parameters

--- src/agents/agent_dqn.py
+++ src/agents/agent_dqn.py
@@ -14,8 +14,8 @@ class DQNAgent:
         self.memory = collections.deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.95
+        self.epsilon_min = 0.30
+        self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
 
