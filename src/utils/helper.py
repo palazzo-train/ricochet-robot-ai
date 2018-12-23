989bf037fce81f8d5e1bf17279a2d38e2f17ae36
Ray Wong <ray.wkwong@gmail.com>
2018-12-23 21:02:52 +0800

    save model

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -3,6 +3,7 @@ from agent_dqn import DQNAgent
 from robot_reboot_extractor import RobotRebootExtractor
 from ricochet_env import RicochetEnv
 
+from keras.models import model_from_json
 
 
 def prepare_game(img_file='game1.png'):
@@ -12,3 +13,33 @@ def prepare_game(img_file='game1.png'):
     env = RicochetEnv(rre)
     
     return agent , env
+
+
+def save_model(model, file_name='model'):
+    model_json_file = file_name + '_model.json'
+    model_weight_file = file_name + '_weight.h5'
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(model_json_file, "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(model_weight_file)
+    print("Saved model to disk")
+    
+
+def load_model(file_name='model'):
+    model_json_file = file_name + '_model.json'
+    model_weight_file = file_name + '_weight.h5'
+    # load json and create model
+    json_file = open(model_json_file, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(model_weight_file)
+    print("Loaded model from disk")
+
+
+    return loaded_model
+
+
