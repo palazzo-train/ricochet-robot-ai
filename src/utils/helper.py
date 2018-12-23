c10b2fc884b6bb3953176457eae746ad92b2a2f6
Ray Wong <ray.wkwong@gmail.com>
2018-12-23 13:59:54 +0800

    fix bug, action size is 16

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -7,7 +7,7 @@ from ricochet_env import RicochetEnv
 
 def prepare_game(img_file='game1.png'):
     im = Image.open( '../games/' + img_file).convert('RGB')
-    agent = DQNAgent(8 , 4)
+    agent = DQNAgent(8 , 4*4)
     rre = RobotRebootExtractor(im)
     env = RicochetEnv(rre)
     
